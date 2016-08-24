f9ccf785eaae5e97d34e8a0e819b77d62550159c
Poren Chiang <ren.chiang@gmail.com>
2016-08-25 00:11:40 +0800

    Migrate to single repository

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            files: 'src/*.less',
+            tasks: ['less']
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ['src/'],
+                    plugins: [
+                        new (require('less-plugin-clean-css'))({ advanced: true, mediaMerging: true })
+                    ],
+                },
+                files: {
+                    'assets/styles.css': 'src/styles.less'
+                }
+            }
+        }
+    });
+
+    // "~ $ grunt" will run the task "watch"
+    grunt.registerTask('default', ['watch']);
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+“2012” Theme
+============
+
+A “classical” theme for NTU Student Weekly. Based on Kytu’s original HTML template
+written in 2012, hence the name.
+
+Contributors
+------------
+
+* [Kytu](https://github.com/kytu800) (@kytu800)
+* [RSChiang](https://github.com/rschiang) (@rschiang)
